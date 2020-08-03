cd6867f1dba72389e4ffd044c1f6d7c74278f784
Shayan Gagan <shayangagan@Shayans-MacBook-Pro.local>
2020-08-03 18:07:09 -0500

    [node]my fist node repo

---
+++ index.js
@@ -0,0 +1,42 @@
+// const http = require('http')
+
+// http.createServer((req, res)=>{
+//     res.write('Hello, world!')
+//     res.end()
+// }).listen(8000)
+
+// // node keyword require allows us to import modules
+// // const myModule = require('./myModule.js')
+// // const fs = require('fs')
+
+// // fs.readFile('story.txt', 'utf8', (err, data)=>{
+// //     if(err) {
+// //         console.log("There was a problem reading the file")
+// //     } else {
+// //         console.log(data)
+// //     }
+// // })
+
+// // console.log(myModule.beBasic())
+
+// // myModule.count()
+
+// // console.log("Hello, World!")
+
+// let i = 1
+
+// const count - () => {
+//     console.log(i)
+//     i++
+// }
+// const myTimer = setInterval(count, 1000)
+
+// const today = new Date()
+
+// console.log(today)
+
+const moment = require('moment')
+
+// console.log(moment().format("MMM Do YYYY"))
+
+console.log(moment("08161996", "MMDDYYYY").format('dddd [the] Do [of] MMMM [in the year] YYYY'))
